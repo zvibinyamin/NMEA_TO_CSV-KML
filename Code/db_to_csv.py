31168a5a43edfeee45dcfdf14fa3ffbca739d64e
sapirankri <sapirankri23@gmail.com>
2016-04-13 00:05:55 +0300

    Added files via upload
    convert database to csv file

---
+++ Code/db_to_csv.py
@@ -0,0 +1,23 @@
+import sqlite3
+import csv
+
+file_name= open("to_csv","w")
+c=csv.writer(file_name)
+
+c.writerow(['date','time','speed', 'latitude', 'lat_direction', 'longitude', 'lon_direction','fix','horizontal','altitude','direct_altitude','altitude_location'])
+
+connection = sqlite3.connect("nmea_to_db.db")
+
+cursor = connection.cursor()
+
+cursor.execute("SELECT * FROM info")
+
+result = cursor.fetchall()
+for r in result:
+    c.writerow(r)
+cursor.execute("SELECT * FROM info")
+
+
+cursor.close()
+file_name.close()
+connection.close()
